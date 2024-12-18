6f507f5d9169ae0f09ee433d2d2cfd5c321c4dd1
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2024-12-18 15:14:42 +0530

    list-voices

--- src/app/components/TTS.jsx
+++ src/app/components/TTS.jsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState, useRef, useEffect } from 'react';
+import {list_voices} from './list-voices'
 
 export default function Home({content}) {
   // const [text, setText] = useState(
@@ -12,28 +13,28 @@ export default function Home({content}) {
   const [loading, setLoading] = useState(false);
   const [autoPlay, setAutoPlay] = useState(true);
   const [playbackSpeed, setPlaybackSpeed] = useState(1.0);
-  const [languages, setLanguages] = useState([]);
-  const [languagesLoading, setLanguagesLoading] = useState(true);
+  const [languages, setLanguages] = useState(list_voices);
+  const [languagesLoading, setLanguagesLoading] = useState(false);
   const audioRef = useRef(null);
 
   // Fetch languages on component mount
-  useEffect(() => {
-    const fetchLanguages = async () => {
-      try {
-        const response = await fetch('/api/list-voices');
-        if (!response.ok) {
-          throw new Error('Failed to fetch languages.');
-        }
-        const data = await response.json();
-        setLanguages(data.languages);
-      } catch (error) {
-        console.error('Error fetching languages:', error);
-      } finally {
-        setLanguagesLoading(false);
-      }
-    };
-    fetchLanguages();
-  }, []);
+  // useEffect(() => {
+  //   const fetchLanguages = async () => {
+  //     try {
+  //       const response = await fetch('/api/list-voices');
+  //       if (!response.ok) {
+  //         throw new Error('Failed to fetch languages.');
+  //       }
+  //       const data = await response.json();
+  //       setLanguages(data.languages);
+  //     } catch (error) {
+  //       console.error('Error fetching languages:', error);
+  //     } finally {
+  //       setLanguagesLoading(false);
+  //     }
+  //   };
+  //   fetchLanguages();
+  // }, []);
 
   const handleSpeak = async () => {
     if (!content.trim()) {
