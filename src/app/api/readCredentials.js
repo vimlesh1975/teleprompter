c89dee6092c6972ed13763e85a02f4fee149ea25
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2025-03-08 16:59:00 +0530

    var mysqlConfig

--- src/app/api/readCredentials.js
+++ src/app/api/readCredentials.js
@@ -2,6 +2,7 @@ import { readFile } from 'node:fs/promises';
 const phpFilePath = 'c:/inetpub/wwwroot/nrcscred.php'; //absolute path to the php file
 // const phpFilePath = '../../nrcscred.php'; //absolute path to the php file
 
+var mysqlConfig = {};
 const readCredentials = async () => {
     try {
         const data = await readFile(phpFilePath, 'utf-8');
@@ -14,7 +15,7 @@ const readCredentials = async () => {
                 return acc;
             }, {});
 
-            const mysqlConfig = {
+            mysqlConfig = {
                 host: variables.servername,
                 user: variables.username,
                 password: variables.password,
@@ -22,12 +23,14 @@ const readCredentials = async () => {
                 connectTimeout: 20000, // 20 seconds
             };
 
-            return { mysqlConfig };
+
         }
     } catch (err) {
         console.error('Error reading PHP file:', err);
-        throw err; // Rethrow the error for handling at a higher level if needed
+        // throw err; // Rethrow the error for handling at a higher level if needed
     }
+
+    return { mysqlConfig };
 };
 
 export default readCredentials;
