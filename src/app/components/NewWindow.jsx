3adb5e66d61062b476516402da1e7de3c0bfa133
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2025-03-12 17:21:55 +0530

    maximize new window with double click in contetent

--- src/app/components/NewWindow.jsx
+++ src/app/components/NewWindow.jsx
@@ -7,7 +7,10 @@ function NewWindow({ children, onClose, newWindowRef, scrollWidth, scrollHeight 
     const [scaleFactor, setScaleFactor] = useState(1);
     // State to keep track of flip status
 
-    const handleTitleBarDoubleClick = () => {
+    const handleTitleBarDoubleClick = (e) => {
+        newWindowRef.current.resizeTo(newWindowRef.current.screen.availWidth, newWindowRef.current.screen.availHeight);
+        newWindowRef.current.moveTo(0, 0);
+
         console.log('Title bar double-clicked');
         container.style.transformOrigin = '5px 0';
         const screenHeight = newWindowRef.current.screen.height;
