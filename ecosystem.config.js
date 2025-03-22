5e6807a52e6b5b1cac1895ec223331da7e78ceda
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2025-03-22 10:21:02 +0530

    ecosystem.config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,14 +3,6 @@ module.exports = {
     {
       name: "Teleprompter",
       script: "server.js",
-      env: {
-        NODE_ENV: "development",
-        PORT: 3000
-      },
-      env_production: {
-        NODE_ENV: "production",
-        PORT: 3000
-      }
     }
   ]
 };
