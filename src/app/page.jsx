6596c79894a3a337e125b4b42bf8dd6297ad1f89
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2025-07-15 11:03:27 +0530

    database visibility

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -941,11 +941,12 @@ export default function Home() {
                   onChange={handleDateChange}
                   disabled={!useDB}
                 />
-                {/* <span title="Database Status"> {(serverAlive && (databaseConnection === 'true')) ? 'Database 🟢' : 'Database 🔴'}</span> */}
+                <span title="Database Status"> {(serverAlive && (databaseConnection === 'true')) ? 'Database 🟢' : 'Database 🔴'}</span>
               </div>
 
             }
-            <span title="Database Status"> {(serverAlive && (databaseConnection === 'true')) ? 'Database 🟢' : 'Database 🔴'}</span>
+            {!newdatabase && <span title="Database Status"> {(serverAlive && (databaseConnection === 'true')) ? 'Database 🟢' : 'Database 🔴'}</span>}
+
 
           </div>
 
