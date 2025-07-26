c2fa98d0d0673857f8b9acf2f2770b5b3575c5b5
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2025-07-26 17:20:11 +0530

    some progress

--- src/app/components/Scroll.jsx
+++ src/app/components/Scroll.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useLayoutEffect } from 'react';
 import io from 'socket.io-client';
 import ScrollView from './ScrollView';
 import { changeStoryLines, changeCrossedLines } from '../store/store';
@@ -10,6 +10,8 @@ import { useDispatch, useSelector } from 'react-redux';
 function moveZerosToFront(arr) {
     return [...arr.filter(n => n === 0), ...arr.filter(n => n !== 0)];
 }
+
+var dd = 0;
 
 const Scroll = ({ scrollContainerStyle, scrollingTextStyle,
     currentFont, fontBold, isRTL, fontColor,
@@ -34,6 +36,9 @@ const Scroll = ({ scrollContainerStyle, scrollingTextStyle,
     const scale = Math.min(scaleX, scaleY); // keep aspect ratio
 
     const scrollContainerRef = useRef(null); // 👈 Add this
+    const prevContentHeight = useRef(0);
+    const prevstoryLines = useRef([]);
+
 
 
     useEffect(() => {
@@ -127,24 +132,27 @@ const Scroll = ({ scrollContainerStyle, scrollingTextStyle,
         return 0;
     };
 
+
+
+
     useEffect(() => {
 
 
-        if (!slugs || !contentRefs.current || !scrollContainerRef.current) return;
+        if (!slugs) return;
 
-        const index = (currentStoryNumber - 1) * 3;
-        const refBefore = contentRefs.current[index];
-        const topBefore = refBefore?.getBoundingClientRect().top;
+        // const index = (currentStoryNumber - 1) * 3;
+        // const refBefore = contentRefs.current[index];
+        // const topBefore = refBefore?.getBoundingClientRect().top;
 
-        requestAnimationFrame(() => {
-            const refAfter = contentRefs.current[index];
-            const topAfter = refAfter?.getBoundingClientRect().top;
+        // requestAnimationFrame(() => {
+        //     const refAfter = contentRefs.current[index];
+        //     const topAfter = refAfter?.getBoundingClientRect().top;
 
-            if (topBefore !== undefined && topAfter !== undefined) {
-                const diff = topAfter - topBefore;
-                scrollContainerRef.current.scrollTop += diff; // Adjust only scroll inside container
-            }
-        });
+        //     if (topBefore !== undefined && topAfter !== undefined) {
+        //         const diff = topAfter - topBefore;
+        //         scrollContainerRef.current.scrollTop += diff; // Adjust only scroll inside container
+        //     }
+        // });
 
         const storiesLines = [];
         for (let i = 0; i < slugs.length * 3; i += 3) {
@@ -161,6 +169,49 @@ const Scroll = ({ scrollContainerStyle, scrollingTextStyle,
         dispatch(changeStoryLines(result));
         socketRef.current.emit('storyLines', result);
     }, [allContent, fontSize, contentRefs, slugs, dispatch]);
+
+    useEffect(() => {
+        prevContentHeight.current = textRef.current.offsetHeight;
+        prevstoryLines.current = [...storyLines];
+
+    }, [doubleClickedPosition])
+    useEffect(() => {
+        const afterHeight = textRef.current?.offsetHeight || 0;
+        const diff = afterHeight - prevContentHeight.current;
+        console.log(diff)
+        // Adjust scroll position
+        if (scrollContainerRef.current) {
+            prevContentHeight.current = afterHeight;
+            dd = dd + 1;
+            console.log(dd)
+
+            const oldArr = prevstoryLines.current;
+            const newArr = storyLines;
+            prevstoryLines.current = [...storyLines]
+            const changed = [];
+
+            for (let i = 0; i < Math.max(oldArr.length, newArr.length); i++) {
+                if (oldArr[i] !== newArr[i]) {
+                    changed.push({
+                        index: i,
+                        oldValue: oldArr[i],
+                        newValue: newArr[i],
+                    });
+                }
+            }
+
+            console.log(oldArr);
+            console.log(newArr);
+            console.log(changed);
+            if (dd < 4) return;
+            if (changed.length === 0) return
+            if (currentStoryNumber < changed.index) return
+
+            setNewPosition((pos) => pos - diff); // Adjust to keep same line visible
+        }
+
+    }, [allContent]); // Trigger after content update
+
 
     return (
         <div style={{ width: scrollWidth, height: scrollHeight, overflow: 'hidden', border: '1px solid black' }}>
